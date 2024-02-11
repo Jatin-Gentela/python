cbc55fce08c1ba0d571af31c1359ff1e23eb45c5
Jatin-Gentela <nvsjatingentela@gmail.com>
2024-02-11 12:51:16 -0500

    adding executable

---
+++ practice.py
@@ -0,0 +1,16 @@
+def function(num1,num2):
+    sum = num1 + num2
+    return sum
+print(function(5,10))
+
+def function(num1,num2):
+    sub = num1 - num2
+    return sub
+print(function(10,5))
+
+
+
+
+
+
+
